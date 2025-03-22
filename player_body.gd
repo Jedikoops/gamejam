36a12132cf3ba66ef569b4e8174b036342dce524
Justin Koops <43502813+Jedikoops@users.noreply.github.com>
2025-03-22 14:52:04 -0400

    movement completed

--- player_body.gd
+++ player_body.gd
@@ -11,48 +11,100 @@ extends CharacterBody2D
 
 var right
 var jump
-const SPEED = 300.0
+const FAST = 300.0
+const SLOW = 100.0
+var speed
 const JUMP_VELOCITY = -400.0
+var jump_velocity
+const MIN_JUMP = -300.0
+const MAX_JUMP = -500.0
+const JUMP_CHARGERATE = 300.0
+
 
 
 func _ready() -> void:
 	right = true
 	_play_idle()
 	jump = false
+	speed = FAST
+	jump_velocity = MIN_JUMP
 
 func _physics_process(delta: float) -> void:
 	
 	var direction := Input.get_axis("move_left", "move_right")
 	
-	if is_on_floor():
-		pass
-		#GroundMovement
-		if Input.is_action_pressed("jump"):
-			jump = true
-			#you might want to make a function to stagger the jump_velocity
-			velocity.y = JUMP_VELOCITY
-			if right:
-				anim_mouse.play("jumpright")
-			else:
-				anim_mouse.play("jumpleft")
-
-		if direction:
-			velocity.x = direction * SPEED
+		#Calculate Direction
+	if  direction:
 			if direction < 0:
-				anim_mouse.play("walkleft")
 				right = false
 			else:
-				anim_mouse.play("walkright")
 				right = true
+	
+	if is_on_floor():
+		#GroundMovement
+		if Input.is_action_just_pressed("jump"):
+			jump = true
+		if Input.is_action_pressed("jump"):
+			#you might want to make a function to stagger the jump_velocity
+			if(jump):
+				if right:
+					anim_mouse.play("jumpreadyright")
+				else:
+					anim_mouse.play("jumpreadyleft")
+			if anim_mouse.frame == 3:
+				jump = false
+			if(!jump):
+				if right:
+					anim_mouse.play("jumpholdright")
+				else:
+					anim_mouse.play("jumpholdleft")
+				
+			
+		
+		elif  direction: #velocity and animations
+			if direction < 0:
+				anim_mouse.play("walkleft")
+			else:
+				anim_mouse.play("walkright")
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
 			if(right):
 				anim_mouse.play("idleright")
 			else:
 				anim_mouse.play("idleleft")
 		
+	else: #not on floor
+		if(velocity.y < 0): #going up
+			if(right):
+				anim_mouse.play("fallupright")
+			else:
+				anim_mouse.play("fallupleft")
+		else:
+			if(right):
+				anim_mouse.play("falldownright")
+			else:
+				anim_mouse.play("falldownleft")
+	
+	
+	#Movement
+	if(is_on_floor()):
+		if(Input.is_action_just_released("jump")):
+			velocity.y = jump_velocity
+			jump_velocity = MIN_JUMP
+			
+		if(Input.is_action_pressed("jump")):
+			speed = move_toward(speed, SLOW, FAST*delta)
+			jump_velocity = move_toward(jump_velocity, MAX_JUMP, delta*JUMP_CHARGERATE)
+		else:
+			speed = FAST
 	else:
 		velocity += get_gravity() * delta
+	
+				
+	if direction: #velocity and animations
+		velocity.x = direction * speed
+	else:
+		if(is_on_floor()):
+			velocity.x = move_toward(velocity.x, 0, FAST)
 
 	#WeaponAttack
 	if Input.is_action_just_pressed("attack"):
