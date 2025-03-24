501be5c062ae9a40928e2f767f81ed0d099a480b
SammyCadian <samyst05@hotmail.com>
2025-03-24 01:04:35 -0400

    Yeehaw More map

--- ca_nnon.gd
+++ ca_nnon.gd
@@ -2,11 +2,13 @@ extends Area2D
 
 @export var projectile: PackedScene
 @export var maxHealth: int = 3
+@export var fireRate: float = 1.0
 @onready var health: int = maxHealth
 @onready var target: Node2D = null
 
 func _ready() -> void:
-	pass
+	$AnimatedSprite2D.play("None")
+	$AttackTimer.wait_time = fireRate
 
 func _process(delta: float) -> void:
 	if target != null:
@@ -37,11 +39,20 @@ func _on_attack_timer_timeout() -> void:
 	var newProj = projectile.instantiate()
 	#print()
 	#newProj.look_at(target.position)
-	var ranging = (target.position - position).length()/4
+	var ranging = (target.position - position).length()/3
 	newProj.linear_velocity = (((target.position + Vector2.UP * ranging) - position).normalized() * 1000)
 	add_child(newProj)
 	pass # Replace with function body.
 
 func death() -> void:
+	$Sprite2D.visible = false
+	$Gun/Sprite2D2.visible = false
+	$AttackTimer.stop()
+	$AnimatedSprite2D.play("kaBoom")
+	$AudioStreamPlayer2D.play()
+	pass # Replace with function body.
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
 	queue_free()
 	pass # Replace with function body.
--- prefabs/can_proj.gd
+++ prefabs/can_proj.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 
 func _ready() -> void:
+	$AudioStreamPlayer2D.play(RandomNumberGenerator.new().randf()*21.0)
 	$AnimatedSprite2D.play("flying")
 	
 func _process(delta: float) -> void:
 	if linear_velocity.y == 0:
 		linear_velocity = Vector2.ZERO
 		rotation = 0
+		$AudioStreamPlayer2D.stream_paused = true
 		$AnimatedSprite2D.play("leaving")
 
 func _hurt(damage):
