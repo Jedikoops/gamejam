9e714b6dd50121880a94cdba7da54646b305e29e
Justin Koops <43502813+Jedikoops@users.noreply.github.com>
2025-03-27 13:35:39 -0400

    updated ui

--- UI/player_ui.gd
+++ UI/player_ui.gd
@@ -1,7 +1,9 @@
 extends Control
 @onready var health_bar: TextureProgressBar = $HealthBar
 	
-	
+func _ready() -> void:
+	health_bar.value = health_bar.max_value
+
 func _update_text(max_health: float, curr_health: float):
 	health_bar.value = 100*(curr_health/max_health)
 	print(curr_health)
