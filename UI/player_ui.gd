66f2f83b36e6d75f357f9858eb34773e3cbd75f5
SammyCadian <samyst05@hotmail.com>
2025-03-30 00:54:20 -0400

    yeehaw

--- UI/player_ui.gd
+++ UI/player_ui.gd
@@ -1,12 +1,22 @@
 extends Control
 @onready var health_bar: TextureProgressBar = $HealthBar
 var my_health
+var timer
 @onready var shake_anim: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
+	timer = 0.0
 	health_bar.value = health_bar.max_value
 	my_health = health_bar.value
 
+func _process(delta: float) -> void:
+	timer += delta
+	var minutes = int(timer / 60.0)
+	var seconds = snapped(timer - (minutes * 60.0), 0.01)
+	var newTime = "%d:" % minutes + "%05.2f" % seconds
+
+	$SpeedRunTimer.text = newTime
+	
 
 func _update_text(max_health: float, curr_health: float):
 	shake_anim.stop()
